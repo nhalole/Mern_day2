99841e637e55610ba07f2f703cab1ad5f6070da6
nhalole <nmhalole@gmail.com>
2024-02-25 12:27:53 +0530

    COmmit 1

---
+++ demo.js
@@ -0,0 +1,9 @@
+console.log("jooooooo")
+
+function checkvalue(){
+  const data = document.demoform.uname.value 
+  if (data=="")
+  document.getElementById('show').innerHTML='name cannot be blank '
+  console.log("data must be tyehre")
+
+}
